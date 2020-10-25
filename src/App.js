876dafdb6510306910780e25044ad5214e1c4174
alexmasturkov <mastrukov@yahoo.ca>
2020-10-25 11:48:47 -0400

    Added new styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
 class App extends Component {
@@ -7,7 +9,9 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: '',
     };
+    //this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -15,10 +19,24 @@ class App extends Component {
       response.json().then((users) => this.setState({ monsters: users }))
     );
   }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((m) =>
+      m.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}></CardList>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
