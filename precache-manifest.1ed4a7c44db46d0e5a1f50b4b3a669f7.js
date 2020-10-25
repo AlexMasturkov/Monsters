f7e71e386035e5d26b198a1d0c7ea2f769deafd0
alexmasturkov <mastrukov@yahoo.ca>
2020-10-25 11:58:08 -0400

    Updates

---
+++ precache-manifest.1ed4a7c44db46d0e5a1f50b4b3a669f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee68b92199828103b95f961760f0e1f4",
+    "url": "/Monsters/index.html"
+  },
+  {
+    "revision": "dd4a58cc7828e860105c",
+    "url": "/Monsters/static/css/main.278351d4.chunk.css"
+  },
+  {
+    "revision": "f446d9444977f330bfbe",
+    "url": "/Monsters/static/js/2.70969f69.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/Monsters/static/js/2.70969f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd4a58cc7828e860105c",
+    "url": "/Monsters/static/js/main.17fc17e7.chunk.js"
+  },
+  {
+    "revision": "99d8a27463727051162c",
+    "url": "/Monsters/static/js/runtime-main.3abc2b3c.js"
+  }
+]);
